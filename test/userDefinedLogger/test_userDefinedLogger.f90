2b54b261abc53dc212084a7f05d6f8f3e14a230c
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-10-15 17:49:33 +0900

    test: added tests for logger_selector

---
+++ test/userDefinedLogger/test_userDefinedLogger.f90
@@ -0,0 +1,185 @@
+module test_mod_userDefinedLogger
+    use, intrinsic :: iso_c_binding
+    use, intrinsic :: iso_fortran_env
+    use :: stdlib_logger, only:logger_type
+    use :: testdrive, only:new_unittest, unittest_type, error_type, check
+    use :: catechin_userDefinedLogger
+    implicit none
+    private
+    public :: collect
+
+contains
+    !>constructing collection of tests.
+    subroutine collect(test_suite)
+        implicit none
+        type(unittest_type), allocatable, intent(out) :: test_suite(:)
+            !! collection of tests
+
+        test_suite = [ &
+                     new_unittest("The trace logger selected by `logger_selecter` &
+                                  &is the same as the defined trace logger", &
+                                  test_logger_selector_trace) &
+                     , new_unittest("The report logger selected by `logger_selecter` &
+                                    &is the same as the defined report logger", &
+                                    test_logger_selector_report) &
+                     , new_unittest("The develop logger selected by `logger_selecter` &
+                                    &is the same as the defined develop logger", &
+                                    test_logger_selector_develop) &
+                     , new_unittest("The measure logger selected by `logger_selecter` &
+                                    &is the same as the defined measure logger", &
+                                    test_logger_selector_measure) &
+                     , new_unittest("The logger returned by `logger_selecter` with an unexpected argument is null", &
+                                    test_logger_selector_null) &
+                     ]
+    end subroutine collect
+
+    !>testing the procedure `[[catechin__logger_selector]]` with the argument `purpose=Purpose_Trace`.
+    !>
+    !>This test is checking
+    !>
+    !>- the returned logger is not null by associating with a pointer and its status.
+    !>- the address of returned logger is the same as it of the corresponding logger defined in `[[catechin_userDefinedLogger]]`.
+    !>
+    subroutine test_logger_selector_trace(error)
+        implicit none
+        type(error_type), allocatable, intent(out) :: error
+            !! error handler
+
+        type(logger_type), pointer :: logger
+
+        logger => catechin__logger_selector(Purpose_Trace)
+
+        call check(error, associated(logger), &
+                   message="logger is not associated")
+        if (allocated(error)) return
+
+        call check(error, c_associated(c_loc(logger), c_loc(trace)), &
+                   message="the address of selected logger is not the same it of defined trace logger")
+        if (allocated(error)) return
+    end subroutine test_logger_selector_trace
+
+    !>testing the procedure `[[catechin__logger_selector]]` with the argument `purpose=Purpose_Report`.
+    !>
+    !>This test is checking
+    !>
+    !>- the returned logger is not null by associating with a pointer and its status.
+    !>- the address of returned logger is the same as it of the corresponding logger defined in `[[catechin_userDefinedLogger]]`.
+    !>
+    subroutine test_logger_selector_report(error)
+        implicit none
+        type(error_type), allocatable, intent(out) :: error
+            !! error handler
+
+        type(logger_type), pointer :: logger
+
+        logger => catechin__logger_selector(Purpose_Report)
+        call check(error, associated(logger), &
+                   message="logger is not associated")
+        if (allocated(error)) return
+
+        call check(error, c_associated(c_loc(logger), c_loc(report)), &
+                   message="the address of selected logger is not the same it of defined report logger")
+        if (allocated(error)) return
+    end subroutine test_logger_selector_report
+
+    !>testing the procedure `[[catechin__logger_selector]]` with the argument `purpose=Purpose_Develop`.
+    !>
+    !>This test is checking
+    !>
+    !>- the returned logger is not null by associating with a pointer and its status.
+    !>- the address of returned logger is the same as it of the corresponding logger defined in `[[catechin_userDefinedLogger]]`.
+    !>
+    subroutine test_logger_selector_develop(error)
+        implicit none
+        type(error_type), allocatable, intent(out) :: error
+            !! error handler
+
+        type(logger_type), pointer :: logger
+
+        logger => catechin__logger_selector(Purpose_Develop)
+        call check(error, associated(logger), &
+                   message="logger is not associated")
+        if (allocated(error)) return
+
+        call check(error, c_associated(c_loc(logger), c_loc(develop)), &
+                   message="the address of selected logger is not the same it of defined develop logger")
+        if (allocated(error)) return
+    end subroutine test_logger_selector_develop
+
+    !>testing the procedure `[[catechin__logger_selector]]` with the argument `purpose=Purpose_Measure`.
+    !>
+    !>This test is checking
+    !>
+    !>- the returned logger is not null by associating with a pointer and its status.
+    !>- the address of returned logger is the same as it of the corresponding logger defined in `[[catechin_userDefinedLogger]]`.
+    !>
+    subroutine test_logger_selector_measure(error)
+        implicit none
+        type(error_type), allocatable, intent(out) :: error
+            !! error handler
+
+        type(logger_type), pointer :: logger
+
+        logger => catechin__logger_selector(Purpose_Measure)
+        call check(error, associated(logger), &
+                   message="logger is not associated")
+        if (allocated(error)) return
+
+        call check(error, c_associated(c_loc(logger), c_loc(measure)), &
+                   message="the address of selected logger is not the same it of defined measure logger")
+        if (allocated(error)) return
+    end subroutine test_logger_selector_measure
+
+    !>testing the procedure `[[catechin__logger_selector]]` with an unexpected argument.
+    !>
+    !>This test is checking
+    !>
+    !>- the returned logger is null by associating with a pointer and its status.
+    !>
+    subroutine test_logger_selector_null(error)
+        implicit none
+        type(error_type), allocatable, intent(out) :: error
+            !! error handler
+
+        type(logger_type), pointer :: logger
+        integer(int32) :: purpose_id
+
+        ! test with arguments in (Int32 minimun,  Int32 maximum]
+        purpose_id = huge(purpose_id)
+        do while (abs(purpose_id) > 0)
+
+            if (purpose_id < Purpose_Trace  .or. &
+                             Purpose_Sentinel <= purpose_id) then !&
+
+                logger => catechin__logger_selector(purpose_id)
+                call check(error,.not. associated(logger), &
+                           message="logger is associated unexpectedly")
+                if (allocated(error)) exit
+
+            end if
+            purpose_id = -purpose_id/2
+
+        end do
+    end subroutine test_logger_selector_null
+end module test_mod_userDefinedLogger
+
+program test_userDefinedLogger
+    use, intrinsic :: iso_fortran_env
+    use :: test_mod_userDefinedLogger
+    use :: testdrive, only:run_testsuite, new_testsuite, testsuite_type
+    implicit none
+    character(len=*), parameter :: fmt = '("#", *(1x, a))'
+    integer(int32) :: stat, test_num
+
+    type(testsuite_type), allocatable :: test_suites(:)
+    stat = 0
+
+    test_suites = [ &
+                  new_testsuite("catechin_userDefinedLogger % catechin__logger_selector", collect) &
+                  ]
+
+    do test_num = 1, size(test_suites)
+        write (error_unit, fmt) "Testing:", test_suites(test_num)%name
+        call run_testsuite(test_suites(test_num)%collect, error_unit, stat)
+    end do
+end program test_userDefinedLogger
